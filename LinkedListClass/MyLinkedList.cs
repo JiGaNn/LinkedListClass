584b196ef5873d196a87a1191c346de207823dca
Eugene <Eugene@DESKTOP-CE9GMNF>
2023-02-16 23:56:39 +0800

    Добавьте файлы проекта.

---
+++ LinkedListClass/MyLinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListClass
+{
+    public class MyLinkedList<T>
+    {
+        public class Item
+        {
+            public T value;
+            public Item prev;
+            public Item next;
+        }
+
+    }
+}
