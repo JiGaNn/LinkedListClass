6f4ca411830cc7850f8281410516302faafae066
Евгений Афанасьев <evgenij29082002@gmail.com>
2023-02-17 01:48:36 +0800

    Contains, AddFirst and ToString

--- LinkedListClass/MyLinkedList.cs
+++ LinkedListClass/MyLinkedList.cs
@@ -13,6 +13,64 @@ namespace LinkedListClass
             public T value;
             public Item prev;
             public Item next;
+
+            public Item(T value)
+            {
+                this.value = value;
+                this.prev = null;
+                this.next = null;
+            }
+        }
+        private Item head = null;
+        private Item tail = null;
+
+        public T First;
+
+        public MyLinkedList(T[] values)
+        {
+            tail = head = new Item(values[0]);
+            First = values[0];
+            Item prev;
+            for(int i = 1; i < values.Length; i++)
+            {
+                tail.next = new Item(values[i]);
+                prev = tail;
+                tail = tail.next;
+                tail.prev = prev;
+            }
+        }
+        public bool Contains(T value)
+        {
+            Item el = head;
+            while (el != null)
+            {
+                if (el.value.Equals(value))
+                {
+                    return true;
+                }
+                el = el.next;
+            }
+            return false;
+        }
+        public void AddFirst(T value)
+        {
+            Item item = new Item(value);
+            item.next = head;
+            head.prev = item;
+            head = item;
+            First = value;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            Item item = head;
+            while (item != null)
+            {
+                str += item.value.ToString();
+                str += " ";
+                item = item.next;
+            }
+            return str;
         }
 
     }
--- LinkedListClass/Program.cs
+++ LinkedListClass/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words =
+            { "the", "fox", "jumps", "over", "the", "dog" };
+            MyLinkedList<string> list = new MyLinkedList<string> (words);
+            Console.WriteLine("sentence.Contains(\"jumps\") = {0}",
+                list.Contains("jumps"));
+
+            list.AddFirst("Why");
+            Console.WriteLine(list.ToString());
+
+            LinkedListNode<string> mark1 = list.First;
+            list.RemoveFirst();
+            list.AddLast(mark1);
         }
     }
 }
